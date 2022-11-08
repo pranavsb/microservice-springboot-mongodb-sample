1c4cfc3ef31ad6c366621efa663bcdfbc7a1c326
Pranav Bijapur <bijapurpranav@gmail.com>
2022-11-08 18:16:23 -0500

    feat: add /getAll endpoint to simulate read heavy workloads

--- sb-accountservice-mongodb/src/main/java/com/hcl/controller/AccountController.java
+++ sb-accountservice-mongodb/src/main/java/com/hcl/controller/AccountController.java
@@ -1,16 +1,11 @@
 package com.hcl.controller;
 
-import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,13 +18,24 @@ import com.hcl.repository.AccountRepository;
 @RequestMapping("/sb")
 public class AccountController {
 
-	@Autowired
-	AccountRepository accountRepository;
-		
-		
-	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public String addAccount(@RequestBody AccountModel accountModel) {
-		accountRepository.save(accountModel);
-		return "Account added successfully";
-	}
+  @Autowired AccountRepository accountRepository;
+
+  @RequestMapping(
+      value = "/add",
+      method = RequestMethod.POST,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public String addAccount(@RequestBody AccountModel accountModel) {
+    accountRepository.save(accountModel);
+    return "Account added successfully";
+  }
+
+  @RequestMapping(
+      value = "/getAll",
+      method = RequestMethod.GET,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Collection<AccountModel>> getAllAccounts() {
+	  List<AccountModel> accounts =
+              (List<AccountModel>) accountRepository.findAll();
+      return ResponseEntity.ok(accounts);
+  }
 }
